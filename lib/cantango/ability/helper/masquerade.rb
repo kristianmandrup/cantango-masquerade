5353e5a27c8cba24af96271400b6d90e12f23609
Kristian Mandrup <kmandrup@gmail.com>
2011-11-30 20:10:09 +0100

    added #subject override to ability helper

--- lib/cantango/ability/helper/masquerade.rb
+++ lib/cantango/ability/helper/masquerade.rb
@@ -2,13 +2,13 @@ module CanTango
   class Ability
     module Helper
       module Masquerade
+        def masquerading?
+          candidate.respond_to?(:masquerading?) && candidate.masquerading?
+        end
+
         def masquerade_user?
           return false if masquerading_off?
           candidate.respond_to?(:active_user) && masquerading?
-        end
-
-        def masquerading?
-          candidate.respond_to?(:masquerading?) && candidate.masquerading?
         end
 
         def masquerade_account?
@@ -19,6 +19,12 @@ module CanTango
         def masquerading_off?
           options[:masquerade] == false
         end
+        
+        def subject
+          return candidate.active_user if masquerade_user?
+          return candidate.active_account if masquerade_account?
+          candidate
+        end        
       end
     end
   end
